c80105d325fc3feed21db004d599e415cbb7b563
Alex Hicks <awh4kc@vt.edu>
2023-12-11 21:19:24 -0500

    added upload json file function

--- src/controllers/ViewController.ts
+++ src/controllers/ViewController.ts
@@ -1,10 +1,65 @@
 import { Request, Response } from 'express';
 import { AppDataSource } from '../db/data-source';
-import { Catalog } from '../db/entities/catalog';
+import { slc_item_catalog } from 'src/db/entities/SLCItemCatalog';
+import logger from 'src/utils/logger';
+import path from 'path';
+import fs from 'fs';
+import { CreateSLCItemDTO } from '../dtos/SLCItemDTO';
+import { validate } from 'class-validator';
+import { ResponseUtil } from 'src/utils/Response';
 
 export class ViewController {
   async homeView(req: Request, res: Response) {
-    const catalog_data = await AppDataSource.getRepository(Catalog).find();
+    const catalog_data = await AppDataSource.getRepository(slc_item_catalog).find();
     res.render('pages/index', { catalog: catalog_data, title: 'SPLICE Catalog' });
+  }
+
+  async uploadView(req: Request, res: Response) {
+    res.render('pages/upload', { title: 'Upload Data' });
+  }
+
+  async uploadPost(req: Request, res: Response) {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    const absolutePath = path.join('/app/', req.file.path);
+    const jsonString = fs.readFileSync(absolutePath, 'utf-8');
+    const jsonObject = JSON.parse(jsonString);
+    let count = 0;
+    let total = 0;
+
+    for (const item in jsonObject) {
+      switch (jsonObject[item].catalog_type) {
+        case 'SLCItemCatalog': {
+          const dto = new CreateSLCItemDTO();
+          Object.assign(dto, jsonObject[item]);
+          const err = await validate(dto);
+          total += 1;
+          if (err.length !== 0) {
+            count += 1;
+            continue;
+            // return ResponseUtil.sendError(res, 'Invalid data', 400, err);
+          } else {
+            const repo = AppDataSource.getRepository(slc_item_catalog);
+            logger.info('item');
+            const CatalogItem = repo.create(jsonObject[item]);
+            logger.info('save');
+            await repo.save(CatalogItem);
+          }
+          break;
+        }
+        case 'DatasetCatalog': {
+          break;
+        }
+        default: {
+          return ResponseUtil.sendResponse(res, req.file, 400);
+        }
+      }
+    }
+    // check if success at end
+    logger.info('count: ' + count);
+    logger.info('total: ' + total);
+    return ResponseUtil.sendResponse(res, req.file, 201);
   }
 }
