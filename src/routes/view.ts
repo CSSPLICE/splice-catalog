672c2b177fd219f14941d62ffaacaa2a1904b88c
Alex Hicks <awh4kc@vt.edu>
2023-12-13 01:14:26 -0500

    fix multer path

--- src/routes/view.ts
+++ src/routes/view.ts
@@ -7,6 +7,9 @@ const viewController = new ViewController();
 const router = express.Router();
 
 const upload = multer({ dest: './upload' });
+if (process.env.NODE_ENV === 'production') {
+    const upload = multer({ dest: './dist/upload' }); 
+  }
 
 router.get('/', viewController.homeView);
 router.post('/upload', upload.single('file'), viewController.uploadPost);
