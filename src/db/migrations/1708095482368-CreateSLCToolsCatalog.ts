26db97c296f56cbe4e1a7c937b5c8d06e9a9b9d1
Yemi Aina <ainababs0@vt.edu>
2024-02-17 10:46:16 -0500

    including migration setup for tools catalog

---
+++ src/db/migrations/1708095482368-CreateSLCToolsCatalog.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSLCToolsCatalog1708095482368 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+            name: "slc_tools_catalog",
+            columns: [
+                {
+                    name: "platform_name",
+                    type: "varchar",
+                    isPrimary: true
+                },
+                {
+                    name: "url",
+                    type: "varchar",
+                },
+                {
+                    name: "tool_description",
+                    type: "text",
+                    isNullable: true,
+                },
+                {
+                    name: "license",
+                    type: "varchar",
+                    isNullable: true,
+                },
+                {
+                    name: "standard_support",
+                    type: "varchar",
+                    isNullable: true,
+                },
+                {
+                    name: "keywords",
+                    type: "text",
+                    isNullable: true,
+                },
+                {
+                    name: "contact_email",
+                    type: "varchar",
+                    isNullable: true,
+                }
+            ]
+        }), true);
+
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
