0e794c089652d3da8ea0dd68bdb981b6b091bef1
Yemi Aina <ainababs0@vt.edu>
2024-03-20 21:31:49 -0400

    including updated migration info

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -2,8 +2,11 @@ import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 import { slc_item_catalog } from './entities/SLCItemCatalog';
 import { slc_tools_catalog } from './entities/SLCToolsCatalog';
-import { CreateSPLICECatalog1699653593228 } from './migrations/1699653593228-CreateSPLICECatalog';
+import { dataset_catalog } from './entities/DatasetCatalog';
+// import { CreateSPLICECatalog1699653593228 } from './migrations/1699653593228-CreateSPLICECatalog';
 import { CreateSLCToolsCatalog1708095482368 } from './migrations/1708095482368-CreateSLCToolsCatalog';
+import { CreateDatasetCatalog1708977188986 } from './migrations/1708977188986-CreateDatasetCatalog';
+import { CreateSpliceCatalog1710861730789 } from './migrations/1710861730789-CreateSpliceCatalog';
 
 dotenv.config();
 
@@ -14,9 +17,9 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER || 'splice',
   password: process.env.DB_PASSWORD || 'splice',
   database: process.env.DB_DATABASE || 'splice',
-  migrations: [CreateSPLICECatalog1699653593228, CreateSLCToolsCatalog1708095482368],
+  migrations: [CreateSpliceCatalog1710861730789, CreateSLCToolsCatalog1708095482368, CreateDatasetCatalog1708977188986],
   logging: process.env.ORM_LOGGING === 'true',
-  entities: [slc_item_catalog, slc_tools_catalog],
+  entities: [slc_item_catalog, slc_tools_catalog, dataset_catalog],
   synchronize: false,
   subscribers: [],
   migrationsRun: true,
