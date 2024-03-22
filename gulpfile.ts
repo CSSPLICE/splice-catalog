eb965e69731422578ec64230bfd5c82a489b8790
Alex Hicks <awh4kc@vt.edu>
2024-03-22 13:43:05 -0400

    fix build pipeling
    Signed-off-by: Alex Hicks <awh4kc@vt.edu>

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 
 gulp.task('assets', function () {
-  return gulp.src('./src/public/**/*.*').pipe(gulp.dest('./dist/public'));
+  return gulp.src('./src/public/**/*.*').pipe(gulp.dest('./dist/src/public'));
 });
 
 gulp.task('views', function () {
-  return gulp.src('./src/views/**/*.ejs').pipe(gulp.dest('./dist/views'));
+  return gulp.src('./src/views/**/*.ejs').pipe(gulp.dest('./dist/src/views'));
 });
 
 gulp.task('default', gulp.series('assets', 'views'), () => {
