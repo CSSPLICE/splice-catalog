aceac8b7f50b3c9b2e7e12e044f7d84ed0cd5235
Yemi Aina <ainababs0@vt.edu>
2024-05-16 23:45:07 -0400

    * implement pagination for search results and updated css

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -6,7 +6,10 @@ import { slc_item_catalog } from '../db/entities/SLCItemCatalog';
 export class SearchController {
   async searchCatalog(req: Request, res: Response) {
     const query = req.body.query;
-    const search_data = await AppDataSource.getRepository(slc_item_catalog).find({
+    const currentPage = Number(req.query.page) || 1;
+    const ITEMS_PER_PAGE = 25; 
+
+    const totalItems = await AppDataSource.getRepository(slc_item_catalog).count({
       where: [
         { keywords: ILike(`%${query}%`) },
         { platform_name: ILike(`%${query}%`) },
@@ -15,6 +18,28 @@ export class SearchController {
         { catalog_type: ILike(`%${query}%`) },
       ],
     });
-    res.render('pages/search', { results: search_data, title: 'Search Results' });
+    const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+
+    const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
+    const search_data = await AppDataSource.getRepository(slc_item_catalog)
+      .find({
+        where: [
+          { keywords: ILike(`%${query}%`) },
+          { platform_name: ILike(`%${query}%`) },
+          { exercise_name: ILike(`%${query}%`) },
+          { exercise_type: ILike(`%${query}%`) },
+          { catalog_type: ILike(`%${query}%`) },
+        ],
+        skip: offset,
+        take: ITEMS_PER_PAGE,
+      });
+
+    res.render('pages/search', {
+      results: search_data,
+      currentPage,
+      totalPages,
+      title: 'Search Results',
+    });
   }
 }
