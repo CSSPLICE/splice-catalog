53664d4db5dba3646cd04c0c190a820e67ad38f7
rishita144 <rishitavalluru@gmail.com>
2025-02-11 11:14:42 -0500

    add function to search for keyword parents to route

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/', searchController.searchCatalog);
 router.get('/', searchController.searchCatalog); //for pagination links
+router.get('/keyword', searchController.searchOntologyByKeyword);
 
 export default router;
