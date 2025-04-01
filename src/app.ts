597ea72b691ec459882ae36fd1a66e7e7d657840
Alex Hicks <awh4kc@vt.edu>
2025-04-01 15:49:40 -0400

    Combine UI updates and new table view

--- src/app.ts
+++ src/app.ts
@@ -70,8 +70,6 @@ app.use('/approve', reviewRoutes);
 
 app.use('/ontology', ontologyRoutes);
 
-app.use('/view', viewRoutes);
-
 app.all('*', (req: Request, res: Response) => {
   return res.status(404).send({
     success: false,
