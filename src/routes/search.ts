2c5d85925c1011cde3a374ab0662b2e92b02b467
manmarilalit <manmari.lalit@gmail.com>
2025-05-06 12:00:47 -0400

    Pitt

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { SearchController } from '../controllers/SearchController';
 
+
 const searchController = new SearchController();
 
 const router = express.Router();
 
 router.post('/', searchController.searchCatalog);
 router.get('/', searchController.searchCatalog); //for pagination links
+router.get('/api', searchController.searchCatalogAPI);
 
 export default router;
