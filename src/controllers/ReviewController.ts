5f40d0e6c04d467d5e2d376663994a147a5bd250
Samartha-m3 <samartha.m3@gmail.com>
2025-10-22 15:58:01 -0400

    cleanup

--- src/controllers/ReviewController.ts
+++ src/controllers/ReviewController.ts
@@ -6,10 +6,7 @@ import { MetadataIssue, CategorizationResult, URLValidationResult } from '../typ
 import { ValidationResults } from '../db/entities/ValidationResults.js';
 import { AppDataSource } from '../db/data-source.js'; // Adjust the path to your data-source file
 import { slc_item_catalog } from '../db/entities/SLCItemCatalog.js';
-import {plainToInstance} from "class-transformer";
-import {CreateSLCItemDTO} from "../dtos/SLCItemDTO.js";
 import {validate, ValidationError} from "class-validator";
-import {error} from "winston";
 
 export class ReviewController {
   async validateAndReview(req: Request, res: Response) {
@@ -36,64 +33,6 @@ export class ReviewController {
       // Process SLCItemCatalogs
       if (slcItemCatalogs.length > 0) {
         logger.info(`Processing ${slcItemCatalogs.length} SLCItemCatalog items`);
-
-        // // Validate metadata
-        // const metadataResult = await validationManager.validateMetadata(slcItemCatalogs);
-        // metadataIssues = metadataResult.issues as MetadataIssue[];
-        // successfulVerifications += metadataResult.successfulVerifications;
-        // totalSubmissions += metadataResult.totalSubmissions;
-        // allValidItems = metadataResult.validItems;
-        //
-        // if (res.locals.io) {
-        //   res.locals.io.emit('metadataValidationComplete', metadataResult);
-        // }
-
-        // // URL Validation
-        // logger.info('Starting URL validation for SLCItemCatalog');
-        // urlResult = await validationManager.validateUrls(allValidItems);
-        // const { urlsChecked, successfulUrls } = urlResult;
-        // if (res.locals.io) {
-        //   res.locals.io.emit('urlValidationComplete', urlResult);
-        // }
-        // logger.info(`URL Validation Completed: ${successfulUrls}/${urlsChecked} successful`);
-
-        // // Categorization Process
-        // logger.info('Starting category report generation for validated items');
-        // const categoryReport = await validationManager.generateCategoryReport(allValidItems);
-        // if (res.locals.io) {
-        //   res.locals.io.emit('categoryReportComplete', categoryReport);
-        // }
-
-        // // Store and classify all items
-        // try {
-        //   logger.info('Starting storage and classification of all valid items');
-        //   await validationManager.storeAndClassifyItems(categoryReport);
-        //   logger.info('Successfully stored and classified all items');
-        // } catch (error) {
-        //   logger.error('Error in storing and classifying items:', error);
-        // }
-
-        // categorizationResults = [
-        //   ...categoryReport.matched.map((item) => ({
-        //     item: item.item.exercise_name,
-        //     status: 'success' as const,
-        //     matchedClass: item.matchedClass,
-        //   })),
-        //   ...categoryReport.unclassified.map((item) => ({
-        //     item: item.item.exercise_name,
-        //     status: 'Unclassified' as const,
-        //     matchedClass: 'Unclassified',
-        //   })),
-        //   ...categoryReport.unmatched.map((item) => ({
-        //     item: item.item.exercise_name,
-        //     status: 'failed' as const,
-        //     matchedClass: 'None',
-        //   })),
-        // ];
-        // if (res.locals.io) {
-        //   res.locals.io.emit('categorizationComplete', categorizationResults);
-        // }
-
         const errors: ValidationError[] = [];
         let saves: number = 0;
         for (const item of slcItemCatalogs) {
