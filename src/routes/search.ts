158246439a070913bf3949c135f8f03eef1cb16c
yixin531 <yixinxie531@vt.edu>
2025-11-11 17:12:10 -0500

    make download button work

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -2,10 +2,9 @@ import express from 'express';
 import { SearchController } from '../controllers/SearchController.js';
 
 const searchController = new SearchController();
-
 const router = express.Router();
 
 router.post('/', searchController.searchCatalog);
 router.get('/', searchController.searchCatalog); //for pagination links
-
+router.get('/export', (req, res) => searchController.exportSearchResults(req, res));
 export default router;
