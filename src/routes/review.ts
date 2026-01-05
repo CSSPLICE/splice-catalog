99c3396d3d8f038bc6b0f353f37f35765cd0bdb3
Samartha-m3 <samartha.m3@gmail.com>
2026-01-05 17:02:27 -0500

    removed all unused services

--- src/routes/review.ts
+++ src/routes/review.ts
@@ -7,7 +7,6 @@ const viewController = new ViewController();
 
 router.get('/catalog', viewController.catalogView);
 router.post('/upload', viewController.uploadPost);
-router.post('/approve', viewController.approveAll);
 router.post('/validate', async (req: Request) => {
   console.log('Received JSON data:', req.body);
   // proceed with validation logic
