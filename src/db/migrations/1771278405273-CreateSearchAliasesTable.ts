130091e896e74fc021bd6a9aad7b5ed29d316077
yixin531 <yixinxie531@vt.edu>
2026-03-06 00:23:36 -0500

    remove MySQL connectio n and syncs from synonums.txt with full match search

--- src/db/migrations/1771278405273-CreateSearchAliasesTable.ts
+++ src/db/migrations/1771278405273-CreateSearchAliasesTable.ts
@@ -5,11 +5,11 @@ export class CreateSearchAliasesTable1771278405273 implements MigrationInterface
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`search_aliases\` (\`id\` int NOT NULL AUTO_INCREMENT, \`term\` varchar(255) NOT NULL, \`synonym\` varchar(255) NOT NULL, \`is_active\` tinyint NOT NULL DEFAULT '1', \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE IF NOT EXISTS \`search_aliases\` (\`id\` int NOT NULL AUTO_INCREMENT, \`term\` varchar(255) NOT NULL, \`synonym\` varchar(255) NOT NULL, \`is_active\` tinyint NOT NULL DEFAULT '1', \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE \`search_aliases\``);
+    await queryRunner.query(`DROP TABLE IF EXISTS \`search_aliases\``);
   }
 }
